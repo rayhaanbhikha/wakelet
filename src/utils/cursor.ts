8efcdcb43360065492089a2f7ac83ff32ecee9cd
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2020-11-25 15:54:29 +0000

    decode cursor functionality refactored.

--- src/utils/cursor.ts
+++ src/utils/cursor.ts
@@ -1,4 +1,13 @@
+import { DynamoDB } from "aws-sdk";
+
 // TODO: should really encrypt cursor.
 export const generateCursor = (data: object) => Buffer.from(JSON.stringify(data)).toString('base64');
 
-export const decodeCursor = (cursor: string) => JSON.parse(Buffer.from(cursor, 'base64').toString('ascii'));
+export const decodeCursor = (cursor: string) => {
+  try {
+    return JSON.parse(Buffer.from(cursor, 'base64').toString('ascii')) as DynamoDB.DocumentClient.Key;
+  } catch (error) {
+    console.error("Unable to parse cursor: ", error.message);
+    return undefined;
+  }
+}
