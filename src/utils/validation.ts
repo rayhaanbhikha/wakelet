006992301357445ff0aa9ee184f183858ce014f7
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2020-11-25 15:54:16 +0000

    validation methods implemented.

---
+++ src/utils/validation.ts
@@ -0,0 +1,14 @@
+type QueryParam = string | string[] | undefined;
+
+export const validateSortBy = (sortBy: QueryParam, defaultValue: string) => {
+  return typeof sortBy === 'string' ? sortBy : defaultValue; 
+}
+
+export const validateOffsetId = (offsetId: QueryParam, defaultValue: string) => {
+  return typeof offsetId === 'string' ? offsetId : defaultValue;
+}
+
+export const validateLimit = (limit: QueryParam, defaultValue: number) => {
+  const numLimit = Number(limit);
+  return typeof numLimit === 'number' && numLimit > 0 ? numLimit : defaultValue
+}
