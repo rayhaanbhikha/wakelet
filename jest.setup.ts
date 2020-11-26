ef3abcf60961187f5f6ea5f8583e53ac71f0192b
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2020-11-26 16:14:44 +0000

    mocking global console in unit tests.

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,9 +1,11 @@
 Object.assign(process.env, {
   PORT: 8080,
-  NASA_EVENTS_BASE_URL: "http://nasa-url.com",
+  NASA_EVENTS_BASE_URL: 'http://nasa-url.com',
   SEED_LIMIT: 10,
   DEFAULT_SCAN_LIMIT: 2,
   ENVIRONMENT: 'dev',
   AWS_REGION: 'us-east-1',
-  DYNAMO_DB_ENDPOINT: 'http://dynamodb:endpoint'
+  DYNAMO_DB_ENDPOINT: 'http://dynamodb:endpoint',
 });
+
+Object.assign(global.console, { log: jest.fn(), error: jest.fn() });
