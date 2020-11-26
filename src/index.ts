a2fde69ef0ceb40a96d601948f7303c23ab83339
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2020-11-26 14:08:30 +0000

    events handler extracted out

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,23 @@
 import express from 'express'
-import { dbService } from './db';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './openapi.json';
 
 import { envs } from './envs';
 import { provisionDB } from './provision';
-import { decodeCursor } from './utils/cursor';
-import { validateLimit, validateOffsetId, validateSortBy } from './utils/validation';
+import { eventsHandler } from './events';
 
 const app = express();
 
 // TODO: unit tests
-// TODO: e2e tests
 // TODO: linting
 
 app.get("/health", (req, res) => {
   res.send("status ok")
 });
 
-app.use('/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.get("/v1/events", async (req, res) => {
-  try {
-
-    const sortBy = validateSortBy(req.query.sortBy as string, '');
-    const offsetId = validateOffsetId(req.query.offsetId as string, '');
-    const scanLimit = validateLimit(req.query.limit as string, envs.DEFAULT_SCAN_LIMIT)
-
-    const cursor = decodeCursor(offsetId);
-    const index = dbService.getIndex(sortBy);
-
-    console.log({ index: index?.name, cursor, limit: scanLimit })
-
-    const result = await dbService.scan({ index, cursor, limit: scanLimit })
-    
-    res.json(result);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ message: "Server error" });
-  }
-});
+app.get("/v1/events", eventsHandler);
 
 const serverInit = async () => {
   try {
